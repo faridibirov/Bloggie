7c1e6443a57325a820ddeb5a57e198795f0d6abb
Farid Dibirov <farid962@gmail.com>
2024-10-16 19:03:52 +0400

    adding tag post method

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers;
 
 public class AdminTagsController : Controller
 {
+    private readonly BloggieDbContext _bloggieDbContext;
+
+    public AdminTagsController(BloggieDbContext bloggieDbContext)
+    {
+        _bloggieDbContext = bloggieDbContext;
+    }
+
     [HttpGet]
     public IActionResult Add()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(AddTagRequest addTagRequest)
+    {
+
+        // Mapping AddTagRequest to TAg domain model
+        var tag = new Tag
+        {
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName
+
+        };
+        
+        _bloggieDbContext.Tags.Add(tag);
+        _bloggieDbContext.SaveChanges();
+
+        return View("Add");
     }
 }
---
+++ Bloggie.Web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Bloggie.Web.Models.ViewModels;
+
+public class AddTagRequest
+{
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+}
